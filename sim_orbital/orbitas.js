6c4657d04df410758ea2dcd3500349f5376e9b9b
Romenig Ribeiro <romenig@gmail.com>
2014-10-04 11:41:21 -0300

    Subindo atualização do Framework.

---
+++ sim_orbital/orbitas.js
@@ -0,0 +1,22 @@
+angular.module('orbitasApp', []).
+	controller('OrbitasController', 
+		['$scope', function($scope) {
+			$scope.corpos = new Array();
+			$scope.nCorpos = 0;
+			$scope.adicionarCorpo = function(){
+				$scope.corpos.push({
+					nome: "Planeta "+$scope.nCorpos,
+					massa: 100,
+					posicao: {x: 0, y:0},
+					cor: 'black'
+				});
+				window.globais.adicionarCorpo($scope.corpos[$scope.corpos.length - 1]);
+				$scope.nCorpos++;
+			}
+
+			$scope.removerCorpo = function(corpo){
+				window.globais.removerCorpo($scope.corpos.indexOf(corpo));
+				$scope.corpos.splice($scope.corpos.indexOf(corpo),1);
+			}
+		}
+	]);
